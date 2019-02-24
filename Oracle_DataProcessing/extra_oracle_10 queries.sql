d6f050a48a610a7689ce9c04dba1bf84accc2900
Stefan Zhang <zhangchi199068@gmail.com>
2019-02-24 16:27:13 -0500

    add 2 (125, 126 )extra long query scripts for test

--- Oracle_DataProcessing/extra_oracle_10 queries.sql
+++ Oracle_DataProcessing/extra_oracle_10 queries.sql
@@ -167,6 +167,42 @@ spool "C:\Users\stefa\Desktop\Work\124.csv"
 exec dbms_output.put_line( (dbms_utility.get_time-:n)/100);
 spool off;
 
+--125 (extra long)
+alter system flush buffer_cache;
+alter system flush shared_pool;
+--counting time
+set serveroutput on
+variable n number
+exec :n := dbms_utility.get_time;
+SELECT MIN(cn1.name) AS first_company, MIN(cn2.name) AS second_company, MIN(mi_idx1.info) AS first_rating, MIN(mi_idx2.info) AS second_rating, MIN(t1.title) AS first_movie, MIN(kt1.kind) AS type, MIN(t2.title) AS second_movie ,MIN(kt2.kind) AS type
+FROM aka_title at1, aka_title at2,company_name cn1, company_name cn2, info_type it1, info_type it2, kind_type kt1, kind_type kt2,  movie_companies mc1, movie_companies mc2, movie_info_idx mi_idx1, movie_info_idx mi_idx2, movie_link  ml, title t1, title t2 
+WHERE it1.info  = 'rating' AND it2.info  = 'rating' AND  mi_idx2.info  < '3.0' AND t2.production_year  <2007 AND at1.production_year >2000 AND at2.production_year >2000
+AND at1.kind_id = t1.kind_id AND at2.kind_id = t2.kind_id AND t1.id = ml.movie_id AND t2.id = ml.linked_movie_id AND it1.id = mi_idx1.info_type_id AND t1.id = mi_idx1.movie_id 
+AND kt1.id = t1.kind_id AND cn1.id = mc1.company_id AND t1.id = mc1.movie_id AND ml.movie_id = mi_idx1.movie_id AND ml.movie_id = mc1.movie_id 
+AND mi_idx1.movie_id = mc1.movie_id AND it2.id = mi_idx2.info_type_id AND t2.id = mi_idx2.movie_id AND kt2.id = t2.kind_id AND cn2.id = mc2.company_id 
+AND t2.id = mc2.movie_id AND ml.linked_movie_id = mi_idx2.movie_id AND ml.linked_movie_id = mc2.movie_id AND mi_idx2.movie_id = mc2.movie_id;
+spool "C:\Users\stefa\Desktop\Work\125.csv"
+exec dbms_output.put_line( (dbms_utility.get_time-:n)/100);
+spool off;
+
+--126 (extra long)
+alter system flush buffer_cache;
+alter system flush shared_pool;
+--counting time
+set serveroutput on
+variable n number
+exec :n := dbms_utility.get_time;
+SELECT MIN(cn1.name) AS first_company, MIN(cn2.name) AS second_company, MIN(mi_idx1.info) AS first_rating, MIN(mi_idx2.info) AS second_rating, MIN(t1.title) AS first_movie, MIN(t2.title) AS second_movie 
+FROM aka_title at1, aka_title at2,company_name cn1, company_name cn2, info_type it1, info_type it2,  movie_companies mc1, movie_companies mc2, movie_info_idx mi_idx1, movie_info_idx mi_idx2, movie_link  ml, title t1, title t2 
+WHERE it1.info  = 'rating' AND it2.info  = 'rating' AND  mi_idx2.info  < '3.0' 
+AND at1.kind_id = t1.kind_id AND at2.kind_id = t2.kind_id AND t1.id = ml.movie_id AND t2.id = ml.linked_movie_id AND it1.id = mi_idx1.info_type_id AND t1.id = mi_idx1.movie_id 
+AND cn1.id = mc1.company_id AND t1.id = mc1.movie_id AND ml.movie_id = mi_idx1.movie_id AND ml.movie_id = mc1.movie_id 
+AND mi_idx1.movie_id = mc1.movie_id AND it2.id = mi_idx2.info_type_id AND t2.id = mi_idx2.movie_id AND cn2.id = mc2.company_id 
+AND t2.id = mc2.movie_id AND ml.linked_movie_id = mi_idx2.movie_id AND ml.linked_movie_id = mc2.movie_id AND mi_idx2.movie_id = mc2.movie_id;
+spool "C:\Users\stefa\Desktop\Work\126.csv"
+exec dbms_output.put_line( (dbms_utility.get_time-:n)/100);
+spool off;
+
 
 
 
